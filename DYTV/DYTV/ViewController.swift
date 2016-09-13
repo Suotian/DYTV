975f05665c46fa351d1a41943426352dd30e0ee2
Suotian <yangtianwei_1@163.com>
2016-09-13 10:19:39 +0800

    first time

---
+++ DYTV/DYTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DYTV
+//
+//  Created by 索田 on 16/9/13.
+//  Copyright © 2016年 ss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
